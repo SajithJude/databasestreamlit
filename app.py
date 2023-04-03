cd1effc77c8b0709b7e8b6e39757d2418e3125b3
Jude Sajith <judesajith.aj@gmail.com>
2023-04-03 15:21:30 +0530

    ssss

--- app.py
+++ app.py
@@ -1,33 +1,18 @@
 import streamlit as st
+import pandas as pd
 import pyodbc
 
-# Initialize connection.
-# Uses st.cache_resource to only run once.
-@st.cache_resource
-def init_connection():
-    return pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-        + st.secrets["server"]
-        + ";DATABASE="
-        + st.secrets["database"]
-        + ";UID="
-        + st.secrets["username"]
-        + ";PWD="
-        + st.secrets["password"]
-    )
+# Establish a connection to the database
+server = 'IP-0A7CE336'
+database = 'bot4'
+username = 'mediawide'
+password = 'DB!@#Pock'
 
-conn = init_connection()
+connection = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
 
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
+# Write a SQL query to retrieve data from the database
+query = "SELECT * FROM YourTable"
+df = pd.read_sql(query, connection)
 
-rows = run_query("SELECT * from mytable;")
-
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+# Display the data in a Streamlit app
+st.dataframe(df)
